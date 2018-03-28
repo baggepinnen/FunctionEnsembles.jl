450ced3b0848e16b78fe5e9ceb7d1b5b87c61e5f
Fredrik Bagge Carlson <cont-frb@ulund.org>
2018-03-28 19:54:38 +0200

    Initial

--- src/FunctionEnsembles.jl
+++ src/FunctionEnsembles.jl
@@ -1,5 +1,45 @@
 module FunctionEnsembles
+using Lazy
+struct Ensemble <: Function
+    funs::Vector{Function}
+end
+Ensemble(e::Ensemble) = e
 
-# package code goes here
+Base.getindex(e::Ensemble, inds...) = e.funs[inds...]
+@forward Ensemble.funs Base.length, Base.size, Base.isempty, Base.getindex, Base.setindex!, Base.push!, Base.deleteat!, Base.insert!, Base.prepend!, Base.start, Base.next, Base.done
+
+
+function (e::Ensemble)(args...; kwargs...)
+    results = map(e.funs) do f
+        f(args...; kwargs...)
+    end
+end
+
+function (e::Ensemble)(op::Function, v0, args...; kwargs...)
+    reduce(op, v0, f(args...; kwargs...) for f in e.funs)
+end
+
+
+macro ensemble(e)
+    @assert e isa Expr "Call syntax: @ensemble v(args...) where v::Vector{Function}"
+    @assert e.head == :call  "Call syntax: @ensemble v(args...) where v::Vector{Function}"
+    quote
+        Ensemble($(esc(e.args[1])))($(esc(e.args[2:end]...)))
+    end
+end
+
+macro functionfirst(e)
+    @assert e isa Expr "Call syntax: @functionfirst fun1(fun2, iterable)"
+    @assert e.head == :call  "Call syntax: @functionfirst fun1(fun2, iterable)"
+    quote
+        outerfun = $(esc(e.args[1]))
+        innerfun = $(esc(e.args[2]))
+        arg = $(esc(e.args[3]))
+        outerfun(innerfun.(arg))
+    end
+end
+
+
+
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,21 @@
 using FunctionEnsembles
 using Base.Test
 
-# write your own tests here
-@test 1 == 2
+v = [sin,cos]
+e = Ensemble(v)
+@test e[1] === v[1] # e behaves very much like a vector
+@test length(e) == length(v)
+
+@test e(1) == @ensemble(v(1)) == @ensemble([sin,cos](1)) == [v[1](1), v[2](1)] # These are all equivalent
+@test @ensemble(e(1)) == e(1)
+
+@test e(+,0,1) == sum(e(1)) # Works like reduce
+
+
+a = randn(4)
+sum(abs2, a)
+mean(abs2, a)
+@test std(abs2.(a)) == @functionfirst std(abs2, a)
+@test std(abs2.(a)) == @functionfirst std(a) do x
+    abs2(x)
+end
